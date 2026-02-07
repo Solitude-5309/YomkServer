5455feb9b11a6c5d1b35c4ae35a33e514618c828
solitude <solitude5309@163.com>
2026-02-07 17:52:16 +0800

    YomkAPI 添加Settings接口

--- Test/YomkServer/TestYomkSettings.cpp
+++ Test/YomkServer/TestYomkSettings.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include "YomkServer.h"
-#include "YomkDefine.h"
+#include "YomkAPI.h"
 
 int main(int argc, char *argv[])
 {
     std::string settingsPath = argv[0] + std::string("/../../Settings/settings.json");
 
-    YomkServer server;
-    server.startService({ 
+    std::shared_ptr<YomkServer> server = std::make_shared<YomkServer>();
+    server->startService({ 
         "/YomkSettings", 
         "/YomkFunctionPool", 
         "/YomkContext",
@@ -15,8 +14,10 @@ int main(int argc, char *argv[])
         "/YomkLogger"
     });
 
+    YOMK_INIT(server);
+
     // load settings
-    YomkResponse response = server.request("/YomkSettings/load", YomkMkYStringPtr(settingsPath));
+    YomkResponse response = YOMK_SETTINGS_LOAD(settingsPath);
     if(response.m_resStatus == YomkResponse::eOk)
     {
         std::cout << "load success" << std::endl;
@@ -25,125 +26,45 @@ int main(int argc, char *argv[])
     {
         std::cout << "load failed: " << response.m_msg << std::endl;
     }
-
     // get cfg_bool
-    response = server.request("/YomkSettings/get", YomkMkYStringPtr("cfg_bool"));
-    if(response.m_resStatus == YomkResponse::eOk)
-    {
-        std::cout << "get cfg_bool success" << std::endl;
-        YomkUnPackPkg(response.m_data, "YSettingBool", YSettingBool, yBool);
-        std::cout << "cfg_bool: " << yBool->d << std::endl;
-    }
-    else
-    {
-        std::cout << "get cfg_bool failed: " << response.m_msg << std::endl;
-    }
-
+    bool cfg_bool = YOMK_SETTINGS_GET_BOOL("cfg_bool");
+    std::cout << "cfg_bool: " << cfg_bool << std::endl;
     // get cfg_bool_array
-    response = server.request("/YomkSettings/get", YomkMkYStringPtr("cfg_bool_array"));
-    if(response.m_resStatus == YomkResponse::eOk)
+    std::vector<bool> cfg_bool_array = YOMK_SETTINGS_GET_BOOL_ARRAY("cfg_bool_array");
+    for(int i = 0; i < cfg_bool_array.size(); i++)
     {
-        std::cout << "get cfg_bool_array success" << std::endl;
-        YomkUnPackPkg(response.m_data, "YSettingBoolArray", YSettingBoolArray, yBoolArray);
-        for(int i = 0; i < yBoolArray->d.size(); i++)
-        {
-            std::cout << "cfg_bool_array[" << i << "] = " << yBoolArray->d[i] << std::endl;
-        }
+        std::cout << "cfg_bool_array[" << i << "] = " << cfg_bool_array[i] << std::endl;
     }
-    else
-    {
-        std::cout << "get cfg_bool_array failed: " << response.m_msg << std::endl;
-    }
-
     // get cfg_double
-    response = server.request("/YomkSettings/get", YomkMkYStringPtr("cfg_double"));
-    if(response.m_resStatus == YomkResponse::eOk)
-    {
-        std::cout << "get cfg_double success" << std::endl;
-        YomkUnPackPkg(response.m_data, "YSettingDouble", YSettingDouble, yDouble);
-        std::cout << "cfg_double: " << yDouble->d << std::endl;
-    }
-    else
-    {
-        std::cout << "get cfg_double failed: " << response.m_msg << std::endl;
-    }
-
+    double cfg_double = YOMK_SETTINGS_GET_DOUBLE("cfg_double");
+    std::cout << "cfg_double: " << cfg_double << std::endl;
     // get cfg_double_array
-    response = server.request("/YomkSettings/get", YomkMkYStringPtr("cfg_double_array"));
-    if(response.m_resStatus == YomkResponse::eOk)
+    std::vector<double> cfg_double_array = YOMK_SETTINGS_GET_DOUBLE_ARRAY("cfg_double_array");
+    for(int i = 0; i < cfg_double_array.size(); i++)
     {
-        std::cout << "get cfg_double_array success" << std::endl;
-        YomkUnPackPkg(response.m_data, "YSettingDoubleArray", YSettingDoubleArray, yDoubleArray);
-        for(int i = 0; i < yDoubleArray->d.size(); i++)
-        {
-            std::cout << "cfg_double_array[" << i << "] = " << yDoubleArray->d[i] << std::endl;
-        }
+        std::cout << "cfg_double_array[" << i << "] = " << cfg_double_array[i] << std::endl;
     }
-    else
-    {
-        std::cout << "get cfg_double_array failed: " << response.m_msg << std::endl;
-    }
-
     // get cfg_int
-    response = server.request("/YomkSettings/get", YomkMkYStringPtr("cfg_int"));
-    if(response.m_resStatus == YomkResponse::eOk)
-    {
-        std::cout << "get cfg_int success" << std::endl;
-        YomkUnPackPkg(response.m_data, "YSettingInt", YSettingInt, yInt);
-        std::cout << "cfg_int: " << yInt->d << std::endl;
-    }
-    else
-    {
-        std::cout << "get cfg_int failed: " << response.m_msg << std::endl;
-    }
-
+    int64_t cfg_int = YOMK_SETTINGS_GET_INT("cfg_int");
+    std::cout << "cfg_int: " << cfg_int << std::endl;
     // get cfg_int_array
-    response = server.request("/YomkSettings/get", YomkMkYStringPtr("cfg_int_array"));
-    if(response.m_resStatus == YomkResponse::eOk)
+    std::vector<int> cfg_int_array = YOMK_SETTINGS_GET_INT_ARRAY("cfg_int_array");
+    for(int i = 0; i < cfg_int_array.size(); i++)
     {
-        std::cout << "get cfg_int_array success" << std::endl;
-        YomkUnPackPkg(response.m_data, "YSettingIntArray", YSettingIntArray, yIntArray);
-        for(int i = 0; i < yIntArray->d.size(); i++)
-        {
-            std::cout << "cfg_int_array[" << i << "] = " << yIntArray->d[i] << std::endl;
-        }
+        std::cout << "cfg_int_array[" << i << "] = " << cfg_int_array[i] << std::endl;
     }
-    else
-    {
-        std::cout << "get cfg_int_array failed: " << response.m_msg << std::endl;
-    }
-
     // get cfg_string
-    response = server.request("/YomkSettings/get", YomkMkYStringPtr("cfg_string"));
-    if(response.m_resStatus == YomkResponse::eOk)
-    {
-        std::cout << "get cfg_string success" << std::endl;
-        YomkUnPackPkg(response.m_data, "YSettingString", YSettingString, yStr);
-        std::cout << "cfg_string: " << yStr->d << std::endl;
-    }
-    else
-    {
-        std::cout << "get cfg_string failed: " << response.m_msg << std::endl;
-    }
-
+    std::string cfg_string = YOMK_SETTINGS_GET_STRING("cfg_string");
+    std::cout << "cfg_string: " << cfg_string << std::endl;
     // get cfg_string_array
-    response = server.request("/YomkSettings/get", YomkMkYStringPtr("cfg_string_array"));
-    if(response.m_resStatus == YomkResponse::eOk)
+    std::vector<std::string> cfg_string_array = YOMK_SETTINGS_GET_STRING_ARRAY("cfg_string_array");
+    for(int i = 0; i < cfg_string_array.size(); i++)
     {
-        std::cout << "get cfg_string_array success" << std::endl;
-        YomkUnPackPkg(response.m_data, "YSettingStringArray", YSettingStringArray, yStrArray);
-        for(int i = 0; i < yStrArray->d.size(); i++)
-        {
-            std::cout << "cfg_string_array[" << i << "] = " << yStrArray->d[i] << std::endl;
-        }
-    }
-    else
-    {
-        std::cout << "get cfg_string_array failed: " << response.m_msg << std::endl;
+        std::cout << "cfg_string_array[" << i << "] = " << cfg_string_array[i] << std::endl;
     }
 
     // set cfg_bool
-    response = server.request("/YomkSettings/set", YomkMkYSettingBoolPtr("cfg_bool", true));
+    response = YOMK_SETTINGS_SET_BOOL("cfg_bool", true);
     if(response.m_resStatus == YomkResponse::eOk)
     {
         std::cout << "set cfg_bool success" << std::endl;
@@ -154,7 +75,7 @@ int main(int argc, char *argv[])
     }
 
     // set cfg_bool_array
-    response = server.request("/YomkSettings/set", YomkMkYSettingBoolArrayPtr("cfg_bool_array", { false, false, false, true, true, true }));
+    response = YOMK_SETTINGS_SET_BOOL_ARRAY("cfg_bool_array", { false, false, false, true, true, true });
     if(response.m_resStatus == YomkResponse::eOk)
     {
         std::cout << "set cfg_bool_array success" << std::endl;
@@ -165,7 +86,7 @@ int main(int argc, char *argv[])
     }
 
     // set cfg_double
-    response = server.request("/YomkSettings/set", YomkMkYSettingDoublePtr("cfg_double", 3.1415926));
+    response = YOMK_SETTINGS_SET_DOUBLE("cfg_double", 3.1415926);
     if(response.m_resStatus == YomkResponse::eOk)
     {
         std::cout << "set cfg_double success" << std::endl;
@@ -176,7 +97,7 @@ int main(int argc, char *argv[])
     }
 
     // set cfg_double_array
-    response = server.request("/YomkSettings/set", YomkMkYSettingDoubleArrayPtr("cfg_double_array", { 100.1, 200.1, 300.1, 400.1, 500.1, 600.1 }));
+    response = YOMK_SETTINGS_SET_DOUBLE_ARRAY("cfg_double_array", { 100.1, 200.1, 300.1, 400.1, 500.1, 600.1 });
     if(response.m_resStatus == YomkResponse::eOk)
     {
         std::cout << "set cfg_double_array success" << std::endl;
@@ -186,8 +107,8 @@ int main(int argc, char *argv[])
         std::cout << "set cfg_double_array failed: " << response.m_msg << std::endl;
     }
 
-    // set cfg_int  
-    response = server.request("/YomkSettings/set", YomkMkYSettingIntPtr("cfg_int", 10086));
+    // set cfg_int
+    response = YOMK_SETTINGS_SET_INT("cfg_int", 10086);
     if(response.m_resStatus == YomkResponse::eOk)
     {
         std::cout << "set cfg_int success" << std::endl;
@@ -198,7 +119,7 @@ int main(int argc, char *argv[])
     }
 
     // set cfg_int_array
-    response = server.request("/YomkSettings/set", YomkMkYSettingIntArrayPtr("cfg_int_array", { 1000, 2000, 3000, 4000, 5000, 6000 }));
+    response = YOMK_SETTINGS_SET_INT_ARRAY("cfg_int_array", { 1000, 2000, 3000, 4000, 5000, 6000 });
     if(response.m_resStatus == YomkResponse::eOk)
     {
         std::cout << "set cfg_int_array success" << std::endl;
@@ -209,7 +130,7 @@ int main(int argc, char *argv[])
     }
 
     // set cfg_string
-    response = server.request("/YomkSettings/set", YomkMkYSettingStringPtr("cfg_string", "1.0.3"));
+    response = YOMK_SETTINGS_SET_STRING("cfg_string", "1.0.3");
     if(response.m_resStatus == YomkResponse::eOk)
     {
         std::cout << "set cfg_string success" << std::endl;
@@ -220,7 +141,7 @@ int main(int argc, char *argv[])
     }
 
     // set cfg_string_array
-    response = server.request("/YomkSettings/set", YomkMkYSettingStringArrayPtr("cfg_string_array", { "aa", "bb", "cc", "dd", "ee", "ff" }));
+    response = YOMK_SETTINGS_SET_STRING_ARRAY("cfg_string_array", { "aa", "bb", "cc", "dd", "ee", "ff" });
     if(response.m_resStatus == YomkResponse::eOk)
     {
         std::cout << "set cfg_string_array success" << std::endl;
@@ -231,7 +152,7 @@ int main(int argc, char *argv[])
     }
 
     // save settings
-    response = server.request("/YomkSettings/save", YomkMkYStringPtr(settingsPath));
+    response = YOMK_SETTINGS_SAVE(settingsPath);
     if(response.m_resStatus == YomkResponse::eOk)
     {
         std::cout << "save success" << std::endl;
--- YomkServer/include/YomkAPI.h
+++ YomkServer/include/YomkAPI.h
@@ -4,6 +4,7 @@
 
 class YomkAPI
 {
+// REQ_API
 public:
     static void init(std::shared_ptr<YomkServer> pServer){
         m_pServer = pServer;
@@ -21,6 +22,8 @@ public:
         }
         return m_pServer->request(path, pkg);
     }
+// LOG_API
+public:
     static YomkResponse CONSOLE_LOG_INFO(const std::string& log, const std::string& tag="MainLogger"){
         if(!m_pServer){
             return YomkResponse(YomkResponse::eInvalid, "YomkServer is not init");
@@ -85,6 +88,164 @@ public:
         }
         return request("/YomkLogger/file_log", YomkMkYLogPtr(YLog::eDebug, "[" + tag + "] " + log, logFile));
     }
+// SETTINGS_API
+public:
+    static YomkResponse SETTINGS_LOAD(const std::string& settingsPath){
+        if(!m_pServer){
+            return YomkResponse(YomkResponse::eInvalid, "YomkServer is not init");
+        }
+        return request("/YomkSettings/load", YomkMkYStringPtr(settingsPath));
+    }
+    static YomkResponse SETTINGS_SAVE(const std::string& settingsPath){
+        if(!m_pServer){
+            return YomkResponse(YomkResponse::eInvalid, "YomkServer is not init");
+        }
+        return request("/YomkSettings/save", YomkMkYStringPtr(settingsPath));
+    }
+    static bool SETTINGS_GET_BOOL(const std::string& key){
+        if(!m_pServer){
+            return false;
+        }
+        YomkResponse response = request("/YomkSettings/get", YomkMkYStringPtr(key));
+        if(response.m_resStatus == YomkResponse::eOk)
+        {
+            YomkUnPackPkg(response.m_data, "YSettingBool", YSettingBool, yBool);
+            return yBool->d;
+        }
+        return false;
+    }
+    static std::vector<bool> SETTINGS_GET_BOOL_ARRAY(const std::string& key){
+        if(!m_pServer){
+            return std::vector<bool>();
+        }
+        YomkResponse response = request("/YomkSettings/get", YomkMkYStringPtr(key));
+        if(response.m_resStatus == YomkResponse::eOk)
+        {
+            YomkUnPackPkg(response.m_data, "YSettingBoolArray", YSettingBoolArray, yBoolArray);
+            return yBoolArray->d;
+        }
+        return std::vector<bool>();
+    }
+    static double SETTINGS_GET_DOUBLE(const std::string& key){
+        if(!m_pServer){
+            return 0.0;
+        }
+        YomkResponse response = request("/YomkSettings/get", YomkMkYStringPtr(key));
+        if(response.m_resStatus == YomkResponse::eOk)
+        {
+            YomkUnPackPkg(response.m_data, "YSettingDouble", YSettingDouble, yDouble);
+            return yDouble->d;
+        }
+        return 0.0;
+    }
+    static std::vector<double> SETTINGS_GET_DOUBLE_ARRAY(const std::string& key){
+        if(!m_pServer){
+            return std::vector<double>();
+        }
+        YomkResponse response = request("/YomkSettings/get", YomkMkYStringPtr(key));
+        if(response.m_resStatus == YomkResponse::eOk)
+        {
+            YomkUnPackPkg(response.m_data, "YSettingDoubleArray", YSettingDoubleArray, yDoubleArray);
+            return yDoubleArray->d;
+        }
+        return std::vector<double>();
+    }
+    static int64_t SETTINGS_GET_INT(const std::string& key){
+        if(!m_pServer){
+            return 0;
+        }
+        YomkResponse response = request("/YomkSettings/get", YomkMkYStringPtr(key));
+        if(response.m_resStatus == YomkResponse::eOk)
+        {
+            YomkUnPackPkg(response.m_data, "YSettingInt", YSettingInt, yInt);
+            return yInt->d;
+        }
+        return 0;
+    }
+    static std::vector<int> SETTINGS_GET_INT_ARRAY(const std::string& key){
+        if(!m_pServer){
+            return std::vector<int>();
+        }
+        YomkResponse response = request("/YomkSettings/get", YomkMkYStringPtr(key));
+        if(response.m_resStatus == YomkResponse::eOk)
+        {
+            YomkUnPackPkg(response.m_data, "YSettingIntArray", YSettingIntArray, yIntArray);
+            return yIntArray->d;
+        }
+        return std::vector<int>();
+    }
+    static std::string SETTINGS_GET_STRING(const std::string& key){
+        if(!m_pServer){
+            return std::string();
+        }
+        YomkResponse response = request("/YomkSettings/get", YomkMkYStringPtr(key));
+        if(response.m_resStatus == YomkResponse::eOk)
+        {
+            YomkUnPackPkg(response.m_data, "YSettingString", YSettingString, yStr);
+            return yStr->d;
+        }
+        return std::string();
+    }
+    static std::vector<std::string> SETTINGS_GET_STRING_ARRAY(const std::string& key){
+        if(!m_pServer){
+            return std::vector<std::string>();
+        }
+        YomkResponse response = request("/YomkSettings/get", YomkMkYStringPtr(key));
+        if(response.m_resStatus == YomkResponse::eOk)
+        {
+            YomkUnPackPkg(response.m_data, "YSettingStringArray", YSettingStringArray, yStrArray);
+            return yStrArray->d;
+        }
+        return std::vector<std::string>();
+    }
+    static YomkResponse SETTINGS_SET_BOOL(const std::string& key, bool value){
+        if(!m_pServer){
+            return YomkResponse(YomkResponse::eInvalid, "YomkServer is not init");
+        }
+        return request("/YomkSettings/set", YomkMkYSettingBoolPtr(key, value));
+    }
+    static YomkResponse SETTINGS_SET_BOOL_ARRAY(const std::string& key, const std::vector<bool>& value){
+        if(!m_pServer){
+            return YomkResponse(YomkResponse::eInvalid, "YomkServer is not init");
+        }
+        return request("/YomkSettings/set", YomkMkYSettingBoolArrayPtr(key, value));
+    }
+    static YomkResponse SETTINGS_SET_DOUBLE(const std::string& key, double value){
+        if(!m_pServer){
+            return YomkResponse(YomkResponse::eInvalid, "YomkServer is not init");
+        }
+        return request("/YomkSettings/set", YomkMkYSettingDoublePtr(key, value));
+    }
+    static YomkResponse SETTINGS_SET_DOUBLE_ARRAY(const std::string& key, const std::vector<double>& value){
+        if(!m_pServer){
+            return YomkResponse(YomkResponse::eInvalid, "YomkServer is not init");
+        }
+        return request("/YomkSettings/set", YomkMkYSettingDoubleArrayPtr(key, value));
+    }
+    static YomkResponse SETTINGS_SET_INT(const std::string& key, int value){
+        if(!m_pServer){
+            return YomkResponse(YomkResponse::eInvalid, "YomkServer is not init");
+        }
+        return request("/YomkSettings/set", YomkMkYSettingIntPtr(key, value));
+    }
+    static YomkResponse SETTINGS_SET_INT_ARRAY(const std::string& key, const std::vector<int>& value){
+        if(!m_pServer){
+            return YomkResponse(YomkResponse::eInvalid, "YomkServer is not init");
+        }
+        return request("/YomkSettings/set", YomkMkYSettingIntArrayPtr(key, value));
+    }
+    static YomkResponse SETTINGS_SET_STRING(const std::string& key, const std::string& value){
+        if(!m_pServer){
+            return YomkResponse(YomkResponse::eInvalid, "YomkServer is not init");
+        }
+        return request("/YomkSettings/set", YomkMkYSettingStringPtr(key, value));
+    }
+    static YomkResponse SETTINGS_SET_STRING_ARRAY(const std::string& key, const std::vector<std::string>& value){
+        if(!m_pServer){
+            return YomkResponse(YomkResponse::eInvalid, "YomkServer is not init");
+        }
+        return request("/YomkSettings/set", YomkMkYSettingStringArrayPtr(key, value));
+    }
 private:
     static std::shared_ptr<YomkServer> m_pServer;
 };
@@ -100,3 +261,21 @@ std::shared_ptr<YomkServer> YomkAPI::m_pServer = nullptr;
 #define YOMK_FILE_WARN(...) YomkAPI::FILE_LOG_WARN(__VA_ARGS__)
 #define YOMK_FILE_ERROR(...) YomkAPI::FILE_LOG_ERROR(__VA_ARGS__)
 #define YOMK_FILE_DEBUG(...) YomkAPI::FILE_LOG_DEBUG(__VA_ARGS__)
+#define YOMK_SETTINGS_LOAD(...) YomkAPI::SETTINGS_LOAD(__VA_ARGS__)
+#define YOMK_SETTINGS_GET_BOOL(...) YomkAPI::SETTINGS_GET_BOOL(__VA_ARGS__)
+#define YOMK_SETTINGS_GET_BOOL_ARRAY(...) YomkAPI::SETTINGS_GET_BOOL_ARRAY(__VA_ARGS__)
+#define YOMK_SETTINGS_GET_DOUBLE(...) YomkAPI::SETTINGS_GET_DOUBLE(__VA_ARGS__)
+#define YOMK_SETTINGS_GET_DOUBLE_ARRAY(...) YomkAPI::SETTINGS_GET_DOUBLE_ARRAY(__VA_ARGS__)
+#define YOMK_SETTINGS_GET_INT(...) YomkAPI::SETTINGS_GET_INT(__VA_ARGS__)
+#define YOMK_SETTINGS_GET_INT_ARRAY(...) YomkAPI::SETTINGS_GET_INT_ARRAY(__VA_ARGS__)
+#define YOMK_SETTINGS_GET_STRING(...) YomkAPI::SETTINGS_GET_STRING(__VA_ARGS__)
+#define YOMK_SETTINGS_GET_STRING_ARRAY(...) YomkAPI::SETTINGS_GET_STRING_ARRAY(__VA_ARGS__)
+#define YOMK_SETTINGS_SET_BOOL(...) YomkAPI::SETTINGS_SET_BOOL(__VA_ARGS__)
+#define YOMK_SETTINGS_SET_BOOL_ARRAY(...) YomkAPI::SETTINGS_SET_BOOL_ARRAY(__VA_ARGS__)
+#define YOMK_SETTINGS_SET_DOUBLE(...) YomkAPI::SETTINGS_SET_DOUBLE(__VA_ARGS__)
+#define YOMK_SETTINGS_SET_DOUBLE_ARRAY(...) YomkAPI::SETTINGS_SET_DOUBLE_ARRAY(__VA_ARGS__)
+#define YOMK_SETTINGS_SET_INT(...) YomkAPI::SETTINGS_SET_INT(__VA_ARGS__)
+#define YOMK_SETTINGS_SET_INT_ARRAY(...) YomkAPI::SETTINGS_SET_INT_ARRAY(__VA_ARGS__)
+#define YOMK_SETTINGS_SET_STRING(...) YomkAPI::SETTINGS_SET_STRING(__VA_ARGS__)
+#define YOMK_SETTINGS_SET_STRING_ARRAY(...) YomkAPI::SETTINGS_SET_STRING_ARRAY(__VA_ARGS__)
+#define YOMK_SETTINGS_SAVE(...) YomkAPI::SETTINGS_SAVE(__VA_ARGS__)
